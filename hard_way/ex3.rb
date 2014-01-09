8651064c46be828588f6dca56b383cdf9bf62fe8
icaroleen <ct@icaroleen.com>
2014-01-09 15:06:17 -0500

    Finished through exercise 12.

--- hard_way/ex3.rb
+++ hard_way/ex3.rb
@@ -1,1 +1,27 @@
+# by adding .0 to end of number, causes that expression to go to floating point
+# require 'mathn' - causes it to go to floating point where applicable
+# by adding .to_f while force that expression to go to floating point
+
 puts "I will now count my chickens:"
+
+puts "Hens", 25 + 30 / 6
+puts "Roosters", 100 - 25 * 3 % 4
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts "Is it ture that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5<= -2
+
